ec64f38fab689d7701789483208ebdae5fced0c6
millenniumhit <ladyautumn11@gmail.com>
2019-10-13 23:31:45 +0300

    parse ip matrix

--- begin1.py
+++ begin1.py
@@ -1,23 +1,19 @@
 import itertools
 from pprint import pprint
 import socket
+from numpy import ndarray
 f = open('test_file.txt', 'r')
 spisok=list()
 for line in f:
     if len(line.strip())>0:
         spisok.append(line.strip())
 new_spis = [el for el, _ in itertools.groupby(spisok)]
-#print(new_spis)
-#pprint(new_spis[2])
-#print(socket.gethostbyname(new_spis[5])[1])
-#pprint(socket.getaddrinfo(new_spis[5], 80)[0])
 
-spisok_ip = [[range(1)], [range(len(new_spis)-1)]]
+spisok_ip = list()
 i = 0
 for ip in new_spis:
-    spisok_ip[0][i] = socket.gethostbyname_ex(ip)[0]
-    spisok_ip[1][i] = socket.gethostbyname_ex(ip)[2]
-    i += 1
-#spisok_ip_n = list()
-#spisok_ip_n = spisok_ip[0], spisok_ip[2]
+    if i < 10: #чтобы не падало на крокозябрах
+        for ip4 in socket.gethostbyname_ex(ip)[2]:
+            spisok_ip.append([socket.gethostbyname_ex(ip)[0], socket.gethostbyname_ex(ip4)[0]])
+            i += 1 #и это тоже убрать
 pprint(spisok_ip)
