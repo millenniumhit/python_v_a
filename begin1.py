37c6fcdb6647fd16704f87045b3d2926e2cc9fa1
millenniumhit <ladyautumn11@gmail.com>
2019-10-13 21:21:31 +0300

    martix ip

--- begin1.py
+++ begin1.py
@@ -8,8 +8,16 @@ for line in f:
         spisok.append(line.strip())
 new_spis = [el for el, _ in itertools.groupby(spisok)]
 #print(new_spis)
-pprint(socket.gethostbyname_ex(new_spis[5]))
 #pprint(new_spis[2])
 #print(socket.gethostbyname(new_spis[5])[1])
 #pprint(socket.getaddrinfo(new_spis[5], 80)[0])
 
+spisok_ip = [[range(1)], [range(len(new_spis)-1)]]
+i = 0
+for ip in new_spis:
+    spisok_ip[0][i] = socket.gethostbyname_ex(ip)[0]
+    spisok_ip[1][i] = socket.gethostbyname_ex(ip)[2]
+    i += 1
+#spisok_ip_n = list()
+#spisok_ip_n = spisok_ip[0], spisok_ip[2]
+pprint(spisok_ip)
