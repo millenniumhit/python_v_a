3846df8c41bf02daad880ec5b1ba4ae55a9e6b7c
millenniumhit <ladyautumn11@gmail.com>
2019-10-13 19:47:17 +0300

    connection socket

--- begin1.py
+++ begin1.py
@@ -1,7 +1,15 @@
 import itertools
+from pprint import pprint
+import socket
 f = open('test_file.txt', 'r')
 spisok=list()
 for line in f:
-    spisok.append(line)
+    if len(line.strip())>0:
+        spisok.append(line.strip())
 new_spis = [el for el, _ in itertools.groupby(spisok)]
-print(new_spis)
+#print(new_spis)
+pprint(socket.gethostbyname_ex(new_spis[5]))
+#pprint(new_spis[2])
+#print(socket.gethostbyname(new_spis[5])[1])
+#pprint(socket.getaddrinfo(new_spis[5], 80)[0])
+
